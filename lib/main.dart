ca4b832ac52852ceaa2c9b440b084333f974a7eb
Samrat Garai <samrat@dailyrounds.org>
2021-05-30 09:19:58 +0530

    added images and intro

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:my_portfolio/strings.dart';
+
+import 'strings.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,70 +26,393 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int selectedMenu = 0;
+  static const int _ITEM_JOURNEY = 0;
+  static const int _ITEM_ARTICLE = 1;
+  static const int _ITEM_CONTACT = 2;
+
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    print(width);
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: null,
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Image.asset('assets/images/home_dev_bg.jpeg', fit: BoxFit.cover),
-          ClipRRect(
-            // Clip it cleanly.
-            child: BackdropFilter(
-              filter: ImageFilter.blur(
-                  sigmaX: 10, sigmaY: 10, tileMode: TileMode.decal),
-              child: Container(
-                color: Colors.white.withOpacity(0.1),
-                alignment: Alignment.topCenter,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 40),
-                      child: Text(
-                        HOME_HEADING,
-                        style: Theme.of(context).textTheme.headline3!.copyWith(
-                              fontWeight: FontWeight.bold,
+      appBar: size.width < 1000 ? AppBar() : null,
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Material(
+                    elevation: 20,
+                    color: Colors.black12,
+                    borderOnForeground: false,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          height: 100,
+                        ),
+                        Container(
+                          child: CircleAvatar(
+                            radius: 150,
+                            backgroundImage:
+                                ExactAssetImage('assets/images/myself.jpg'),
+                          ),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: new Border.all(
+                              color: Colors.white70,
+                              width: 8.0,
                             ),
-                        textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            MenuItem(
+                                MENU_JOURNEY, selectedMenu == _ITEM_JOURNEY,
+                                () {
+                              setState(() {
+                                selectedMenu = _ITEM_JOURNEY;
+                              });
+                            }),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            MenuItem(
+                                MENU_ARTICLE, selectedMenu == _ITEM_ARTICLE,
+                                () {
+                              setState(() {
+                                selectedMenu = _ITEM_ARTICLE;
+                              });
+                            }),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            MenuItem(
+                                MENU_CONTACT, selectedMenu == _ITEM_CONTACT,
+                                () {
+                              setState(() {
+                                selectedMenu = _ITEM_CONTACT;
+                              });
+                            }),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 100,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    color: Colors.white.withOpacity(0.9),
+                    child: SingleChildScrollView(
+                      child: Container(
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 60,
+                            ),
+                            ProjectDetails(
+                              iconUrl: PROJ_MARROW_ICON,
+                              projTitle: PROJ_MARROW,
+                              projSubTitle: PROJ_MARROW_DATE,
+                              roles: PROJ_MARROW_KEY_ROLES,
+                              projIntro: PROJ_MARROW_INTRO,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 60),
+                              child: Divider(
+                                height: 10,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            ProjectDetails(
+                              iconUrl: PROJ_WALL_STREET_ICON,
+                              projTitle: PROJ_WALL_STREET,
+                              projSubTitle: PROJ_WALL_STREET_DATE,
+                              isOffline: true,
+                              roles: PROJ_WALL_STREET_KEY_ROLES,
+                              projIntro: PROJ_WALL_STREET_INTRO,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 60),
+                              child: Divider(
+                                height: 10,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            ProjectDetails(
+                              iconUrl: PROJ_CROSS_HELMET_ICON,
+                              projTitle: PROJ_CROSS_HELMET,
+                              projSubTitle: PROJ_CROSS_HELMET_DATE,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 60),
+                              child: Divider(
+                                height: 10,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            ProjectDetails(
+                              iconUrl: PROJ_FOLX_ICON,
+                              projTitle: PROJ_FOLX,
+                              projSubTitle: PROJ_FOLX_DATE,
+                              isOffline: true,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 60),
+                              child: Divider(
+                                height: 10,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            ProjectDetails(
+                              iconUrl: PROJ_CROTON_ICON,
+                              projTitle: PROJ_CROTON,
+                              projSubTitle: PROJ_CROTON_DATE,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 60),
+                              child: Divider(
+                                height: 10,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            ProjectDetails(
+                              iconUrl: PROJ_WHATS_EASY_ICON,
+                              projTitle: PROJ_WHATS_EASY,
+                              projSubTitle: PROJ_WHATS_EASY_DATE,
+                              isOffline: true,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 60),
+                              child: Divider(
+                                height: 10,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            ProjectDetails(
+                              iconUrl: PROJ_ETEXT_ICON,
+                              projTitle: PROJ_ETEXT,
+                              projSubTitle: PROJ_ETEXT_DATE,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 60),
+                              child: Divider(
+                                height: 10,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            ProjectDetails(
+                              iconUrl: PROJ_PLUGINS_ICON,
+                              projTitle: PROJ_PLUGINS,
+                              projSubTitle: PROJ_PLUGINS_DATE,
+                              isOffline: true,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 60),
+                              child: Divider(
+                                height: 10,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 24),
-                      child: Row(children: [
-                        Spacer(
-                          flex: 2,
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Text(
-                            HOME_SUBHEADING,
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline5!
-                                .copyWith(
-                                    fontWeight: FontWeight.bold,
-                                    fontStyle: FontStyle.italic),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        Visibility(
-                          visible: width > 1000,
-                          child: Spacer(
-                            flex: 1,
-                          ),
-                        ),
-                      ]),
-                    ),
-                  ],
-                ),
-              ),
+                  ),
+                )
+              ],
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  final String itemName;
+  final bool isSelected;
+  final Function callback;
+
+  const MenuItem(this.itemName, this.isSelected, this.callback, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkResponse(
+        hoverColor: Colors.white70.withOpacity(0.1),
+        child: Text(
+          itemName,
+          style: Theme.of(context).textTheme.headline5!.copyWith(
+              color: isSelected ? Colors.white : Colors.grey,
+              fontWeight: FontWeight.bold),
+        ),
+        onTap: () => callback());
+  }
+}
+
+class ProjectDetails extends StatelessWidget {
+  final String iconUrl;
+  final String projTitle;
+  final String projSubTitle;
+  final bool isOffline;
+  final List<String>? roles;
+  final String projIntro;
+
+  const ProjectDetails(
+      {this.iconUrl = "",
+      this.projTitle = "",
+      this.projSubTitle = "",
+      this.isOffline = false,
+      this.roles,
+      this.projIntro = "",
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 75,
+              width: 75,
+              child: isOffline
+                  ? Image.asset(iconUrl)
+                  : Image.network(
+                      iconUrl,
+                    ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  projTitle,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline3!
+                      .copyWith(color: Colors.black87),
+                ),
+                Text(
+                  projSubTitle,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline5!
+                      .copyWith(color: Colors.black45),
+                )
+              ],
+            ),
+          ],
+        ),
+        ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: 800,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  projIntro,
+                  style: Theme.of(context).textTheme.headline5!.copyWith(
+                        color: Colors.black87,
+                      ),
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+              if (roles != null)
+                RolesWidget(
+                  roles: roles,
+                )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class RolesWidget extends StatelessWidget {
+  final List<String>? roles;
+  const RolesWidget({this.roles, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          MENU_ROLES,
+          style: Theme.of(context)
+              .textTheme
+              .headline5!
+              .copyWith(color: Colors.black87),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          children: [
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: roles!
+                    .map<Widget>(
+                      (option) => Padding(
+                        padding: EdgeInsets.symmetric(vertical: 4),
+                        child: Row(children: [
+                          Icon(
+                            Icons.double_arrow,
+                            color: Colors.cyan,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          ConstrainedBox(
+                            constraints: BoxConstraints(maxWidth: 700),
+                            child: Text(
+                              option,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyText2!
+                                  .copyWith(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w200,
+                                    color: Colors.black87,
+                                  ),
+                            ),
+                          ),
+                        ]),
+                      ),
+                    )
+                    .toList()),
+          ],
+        ),
+      ],
     );
   }
 }
--- lib/strings.dart
+++ lib/strings.dart
@@ -7,3 +7,72 @@ const String HOME_LIKE_SKETCHING =
     "I like sketching. Checkout some of my creations here";
 const String HOME_LIKE_ANIME =
     "I like reading manga and anime. Big fan of One Piece.";
+
+const String MENU_JOURNEY = "My Experience";
+const String MENU_ARTICLE = "Blogs";
+const String MENU_CONTACT = "Contact Me";
+
+const String MENU_ROLES = "Roles and Responsibilities - ";
+
+const String PROJ_MARROW = "Marrow";
+const String PROJ_MARROW_ICON =
+    "https://play-lh.googleusercontent.com/oM0UXqkgh0kpgc2alHKdei1Z4R1ZVhVnwkTnAoUeQygq3J0X2EvXInzXCbqz_dSDEcE=s180-rw";
+const String PROJ_MARROW_DATE = "Working from Apr. 2019";
+const String PROJ_MARROW_INTRO =
+    "I started working in this startup here with only a year experience. So, far the journey has been incredible - We have been maintaining MVP architecture with separate data layer and unit test cases on repository and presenter.";
+const List<String> PROJ_MARROW_KEY_ROLES = [
+  "Lead a team of 3 developers",
+  "Data analysis on issues to improve apps performance",
+  "Increased and consistently maintained crash free users from 98% to 99.4% over 7 day period",
+  "Setting up the new base architecture, to easily maintain and scale app over time",
+  "Creating and updating features",
+  "Taking part in decision making team",
+  "Making responsive UI, and improved legacy screens in terms of UI",
+  "Leading Kotlin conversion of existing code base. Its not just convert by using tool :-D"
+];
+
+const String PROJ_WALL_STREET = "Wall Street";
+const String PROJ_WALL_STREET_ICON = "assets/images/wallstreet.png";
+const String PROJ_WALL_STREET_DATE = "Working from Dec. 2020";
+const String PROJ_WALL_STREET_INTRO =
+    "It's based on Beer Bar Stock Exchange. With less than a year of flutter experience I started on this freelance proj. Made 4 separate apps - app for user, staff, admin and display(Android TV). Maintained BLOC architecture, have used Firebase services as backend";
+const List<String> PROJ_WALL_STREET_KEY_ROLES = [
+  "Designed the UI-UX of all the apps and implemented them",
+  "Implemented CRUD operations on Orders",
+  "Had setup the complete Firebase cloud storage db",
+  "Implemented a CRON job to vary price based on product demand",
+  "Integrated a bluetooth based thermal printer billing setup",
+  "Added a dashboard to view or save the sales report",
+  "Added a feature to authenticate each order placement"
+];
+const String PROJ_CROSS_HELMET = "Cross Helmet";
+const String PROJ_CROSS_HELMET_ICON =
+    "https://play-lh.googleusercontent.com/LgHq_QjlK1CV6wklC49x7IUu1-k__aQpCfHyOt-7qXKxrsfy4uf1yj409E_ZVeDY4Q=s180-rw";
+const String PROJ_CROSS_HELMET_DATE = "Working from Sept. 2020";
+
+const String PROJ_FOLX = "Folx";
+const String PROJ_FOLX_ICON = "assets/images/folx.png";
+const String PROJ_FOLX_DATE = "Working from Nov. 2020";
+
+const String PROJ_CROTON = "Croton";
+const String PROJ_CROTON_ICON =
+    "https://play-lh.googleusercontent.com/U0KMb1T-qGKF-BkyqDWiZdEgvgVp5utHAi7TIpW-NiLd-TG8pq_dh9zvLZ96UoRr6XJJ=s180-rw";
+const String PROJ_CROTON_DATE = "Sept. 2019 - Mar. 2020";
+
+const String PROJ_WHATS_EASY = "Whats Easy";
+const String PROJ_WHATS_EASY_ICON = "assets/images/whats_easy.png";
+const String PROJ_WHATS_EASY_DATE = "A day in Mar 2021:-)";
+
+const String PROJ_ETEXT = "eText";
+const String PROJ_ETEXT_ICON =
+    "https://play-lh.googleusercontent.com/IINCgeuyd-R06k0NwGiEf3qGz67eMFZff1M2tJicE5UPDAzVTt6QyxA4PKZsL1d-DfE=s180-rw";
+const String PROJ_ETEXT_DATE = "Jan. 2018 - Apr. 2019";
+
+const String PROJ_PLUGINS = "Plugins for eText and Revel";
+const String PROJ_PLUGINS_ICON = "assets/images/plugins.png";
+const String PROJ_PLUGINS_DATE = "Jan. 2018 - Apr. 2019";
+
+const String ORG_MARROW = "Neuroglia Health Pvt Ltd";
+const String ORG_CTS = "Cognizant Technology Solutions";
+const String ORG_PERSONAL_PROJ = "Personal Project";
+const String ORG_FREELANCING = "Worked as Freelancer";
